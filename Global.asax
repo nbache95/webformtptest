0c0ff5f7d98b94f592ef4e5b9e27ea186f86fb7d
Nour Bache <nour.bache@etu.cyu.fr>
2021-03-11 16:26:28 +0100

    Ajoutez des fichiers projet.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="webformtptest.Global" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(webformtptest.Startup))]
+namespace webformtptest
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
